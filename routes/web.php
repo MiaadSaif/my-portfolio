249bd1a2b96faaedf43460bffb7115330b2d231c
Miaad Al-farsi <118962191+MiaadSaif@users.noreply.github.com>
2025-08-17 01:42:01 +0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ Route::get('/', [PageController::class, 'index'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
 Route::get('/work', [PageController::class, 'work'])->name('work');
 Route::get('/contact', [PageController::class, 'contact'])->name('contact');
-Route::get('/', [PageController::class, 'index'])->name('portfolio');
+/* Route::get('/', [PageController::class, 'index'])->name('portfolio'); */
 Route::get('/project-details/{id}', [PageController::class, 'getProjectDetails']);
 
